1552bbe1262ca5c7b130d60151892ef18af7a9f2
Jorman Garcia <jagarcia@lfs-inc.com>
2023-09-28 05:23:19 -0400

    add logic into the GifGrid.jsx to show the title from images

--- src/Components/GifGrid.jsx
+++ src/Components/GifGrid.jsx
@@ -1,19 +1,37 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
 
+     const [images, setImages] = useState ([]);
+
+     const getImages = async() => {
+      const newImages = await getGifs( category );
+      setImages(newImages);
+    }
+  
+
 
     useEffect( () =>{
-    getGifs(category);
+    getImages();
     }, [ ])
   
   return (
     <>
       <h3>{category}</h3>
+      <o>
+        {
+          images.map(( {id, title }) => (
+            <li key={ id }>{title}</li>
+            )) 
+        }
+        
+        
+
+      </o>
     </>
   )
 }
