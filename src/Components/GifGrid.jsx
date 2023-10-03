0e53446e117a48c2dca4860681ee8cb0180f4649
Jorman Garcia <jagarcia@lfs-inc.com>
2023-10-03 18:13:45 -0400

    changes in the GifGrid and Hook

--- src/Components/GifGrid.jsx
+++ src/Components/GifGrid.jsx
@@ -4,29 +4,19 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import GitItem from './GitItem';
-//import { getGifs } from "../helpers/getGifs";
 import { useFetchGifs } from '../hooks/useFetchGifs';
 
 export const GifGrid = ({ category }) => {
 
   const {images, isLoading} = useFetchGifs( category );
 
-  console.log({images, isLoading});
-
-    //  const [images, setImages] = useState ([]);
-
-    //  const getImages = async() => {
-    //   const newImages = await getGifs( category );
-    //   setImages(newImages);
-    // }
-
-    // useEffect( () =>{
-    // getImages();
-    // }, [ ])
-
   return (
     <>
       <h3>{category}</h3>
+      {
+         isLoading &&  (<h2>Cargando...</h2>)
+      }
+
       <div className="card-grid">
         {
           images.map(( image ) => (
